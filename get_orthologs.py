c1e3309b9fcd9cf759c1df39135296d49565ba96
marianaql <33430291+marianaql@users.noreply.github.com>
2018-06-15 12:07:55 +0200

    Update get_orthologs.py

--- get_orthologs.py
+++ get_orthologs.py
@@ -1,37 +1,58 @@
-##AUTHORS: Jordi Busoms & Mariana Quiroga 
+###################################################################
+
+#Authors: Mariana Quiroga, Jordi Busoms
+
+###################################################################
+
+#Used libraries:
 
 import os
 import sys
 from Bio import SeqIO
 
-with open(sys.argv[1]) as out: #sys.argv[1] that will contain the Blast results
-	IDS={}
-	try:
-		Orthologs_num = sys.argv[3] #sys.argv[3] is the number of hits from which you will take a sequence per each.
-                #If it does not find anything, it will take 20 sequences by default.
+#Optional arguments: Desired_number_of_hits_retrieved, blast_results_file, database_file. To specify a file all arguments must be given
+#Default values: 20, results.out, blast_database_FCG2018.fasta
+try:
+	Orthologs_num= sys.argv[1]
+except:
+	Orthologs_num=20
+try:
+	results_file=sys.argv[2]
+	database_file=sys.argv[3]
+except:
+	results_file="results.out"
+	database_file = "blast_database_FCG2018.fasta"
 
-	except:
-		Orthologs_num = 20
+
+with open(results_file) as out:
+	IDS={}			# IDS will contain all the target IDs asociated with its list of orthologs
+
 	for line in out:
 		if line.split()[0] not in IDS:
-			IDS[line.split()[0]] = []
-		if len(IDS[line.split()[0]])<Orthologs_num:
-			if line.split()[0] != line.split()[1]:
-				IDS[line.split()[0]].append(line.split()[1])
+			IDS[line.split()[0]]=[]
+		if len(IDS[line.split()[0]])<Orthologs_num+1:	#number of horthologs+ itself
+			#if line.split()[0] != line.split()[1]:		#uncomment if do not want to find the target sequence itself.
+			IDS[line.split()[0]].append(line.split()[1])
 
-line=""
-records = list(SeqIO.parse("blast_database_FCG2018.fasta","fasta"))
-All_ids=set()
 
-for ID2 in IDS[sys.argv[2]]: #The target sequence from which you want to print the hits.
-	All_ids.add(ID2)
+records = list(SeqIO.parse(database_file,"fasta"))
+
+All_ids={}			# All_ids will contain all horthologs id associated with the querry it is hortholog to.
+
+for ID1 in IDS:
+	for ID2 in IDS[ID1]:
+		All_ids[ID2]=ID1
 		
-#This is how we do the parsing of the fasta format
+
+
+###PARSING THE FASTA FILE
+files={}			# will contain all taret ids with its file names.
+for ID in IDS:
+	files[ID]=open((str(ID)+'.ortho'), 'w')
+print(files)
 for record in records:
     ID= record.id
     if ID in All_ids:
-		print('>'+record.id)
-		print(record.seq)
-    
-
-
+        print(All_ids[ID])
+        files[All_ids[ID]].write(('>'+record.id+'\n'))
+        files[All_ids[ID]].write(str(record.seq +'\n'))
